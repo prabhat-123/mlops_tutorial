bf4fd9777784c9c6f61af26da2db94a06880ceae
prabhat-123 <srv.ale52@gmail.com>
2022-07-04 01:19:37 +0545

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It's a project to identify whether the sensors is working properly or not.(meaning that the sensor is giving errors or wrong data or not).',
+    author='c17hawke',
+    license='MIT',
+)
